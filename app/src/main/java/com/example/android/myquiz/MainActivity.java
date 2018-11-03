ccfa84bba110ce55f40190548dbb51c08ad2166f
Andre Arruda <andre.luizarruda@ufrpe.br>
2018-11-03 00:29:29 -0300

    exclusão de variáveis desnecessárias

--- app/src/main/java/com/example/android/myquiz/MainActivity.java
+++ app/src/main/java/com/example/android/myquiz/MainActivity.java
@@ -9,41 +9,12 @@ import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
-    RadioGroup pergunta1RadioGroup;
-    int idRespostaCertaPergunta1;
-
-    RadioGroup pergunta2RadioGroup;
-    int idRespostaCertaPergunta2;
-
-    RadioGroup pergunta3RadioGroup;
-    int idRespostaCertaPergunta3;
-
-    RadioGroup pergunta4RadioGroup;
-    int idRespostaCertaPergunta4;
-
-    RadioGroup pergunta5RadioGroup;
-    int idRespostaCertaPergunta5;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //encontra os ids das respostas corretas de cada questão
-        pergunta1RadioGroup = (RadioGroup)findViewById(R.id.pergunta1RadioGroup);
-        idRespostaCertaPergunta1 = R.id.p1AlternativaDRadioButton;
-
-        pergunta2RadioGroup = (RadioGroup)findViewById(R.id.pergunta2RadioGroup);
-        idRespostaCertaPergunta1 = R.id.p2AlternativaARadioButton;
-
-        pergunta3RadioGroup = (RadioGroup)findViewById(R.id.pergunta3RadioGroup);
-        idRespostaCertaPergunta1 = R.id.p3AlternativaBRadioButton;
-
-        pergunta4RadioGroup = (RadioGroup)findViewById(R.id.pergunta4RadioGroup);
-        idRespostaCertaPergunta1 = R.id.p4AlternativaDRadioButton;
-
-        pergunta5RadioGroup = (RadioGroup)findViewById(R.id.pergunta5RadioGroup);
-        idRespostaCertaPergunta1 = R.id.p5AlternativaCRadioButton;
+        
     }
 
     public void enviar(View view){
